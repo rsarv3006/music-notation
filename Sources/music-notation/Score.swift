cd9123a201e8b47eee2e60c0cc04ecc0214e83a8
Robby Sarvis <rjs.app.dev@gmail.com>
2023-02-14 21:47:49 -0600

    further modifications for observable

--- Sources/music-notation/Score.swift
+++ Sources/music-notation/Score.swift
@@ -13,32 +13,32 @@ import Foundation
 public class Score: RandomAccessCollection {
 	// MARK: - Collection Conformance
 
-	public typealias Index = Int
-	public var startIndex: Int								{ parts.startIndex }
-	public var endIndex: Int								{ parts.endIndex }
-	public subscript(position: Index) -> Iterator.Element	{ parts[position] }
-	public func index(after index: Int) -> Int				{ parts.index(after: index) }
-	public func index(before index: Int) -> Int 			{ parts.index(before: index) }
+    public typealias Index = Int
+    public var startIndex: Int								{ parts.startIndex }
+    public var endIndex: Int								{ parts.endIndex }
+    public subscript(position: Index) -> Iterator.Element	{ parts[position] }
+    public func index(after index: Int) -> Int				{ parts.index(after: index) }
+    public func index(before index: Int) -> Int 			{ parts.index(before: index) }
 	public typealias Iterator = IndexingIterator<[Part]>
 	public func makeIterator() -> Iterator 					{ parts.makeIterator() }
 
 	// MARK: - Main Properties
 
-	internal private(set) var parts: [Part] = []
+    @Published internal private(set) var parts: [Part] = []
 	@Published public var title: String = ""
-	public var subTitle: String = ""
-	public var artist: String = ""
-	public var album: String = ""
-	public var words: String = ""
-	public var music: String = ""
-	public var wordsAndMusic: String = ""
-	public var transcriber: String = ""
-	public var instructions: String = ""
-	public var notices: String = ""
+    @Published public var subTitle: String = ""
+    @Published public var artist: String = ""
+    @Published public var album: String = ""
+    @Published public var words: String = ""
+    @Published public var music: String = ""
+    @Published public var wordsAndMusic: String = ""
+    @Published public var transcriber: String = ""
+    @Published public var instructions: String = ""
+    @Published public var notices: String = ""
 
-	public var firstPageHeader: PageHeader?
-	public var pageHeader: PageHeader?
-	public var pageFooter: PageFooter?
+    @Published public var firstPageHeader: PageHeader?
+    @Published public var pageHeader: PageHeader?
+    @Published public var pageFooter: PageFooter?
 
 	public init(parts: [Part]) {
 		self.parts = parts
@@ -52,3 +52,5 @@ extension Score: CustomDebugStringConvertible {
 		return "parts(\(partsDescription))"
 	}
 }
+
+extension Score: ObservableObject {}
